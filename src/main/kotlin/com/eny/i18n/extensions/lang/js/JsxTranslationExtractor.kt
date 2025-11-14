fc55c49a4c339f27313e5944153bc42155067da7
Paulo Cesar Hernandez Rosado <paulocesarhero@gmail.com>
2025-11-13 19:46:52 -0600

    Update plugin version to 2.8.5 and enhance JSX file type detection in JsxTranslationExtractor

--- src/main/kotlin/com/eny/i18n/extensions/lang/js/JsxTranslationExtractor.kt
+++ src/main/kotlin/com/eny/i18n/extensions/lang/js/JsxTranslationExtractor.kt
@@ -2,48 +2,44 @@ package com.eny.i18n.extensions.lang.js
 
 import com.eny.i18n.plugin.factory.TranslationExtractor
 import com.eny.i18n.plugin.utils.toBoolean
-import com.intellij.lang.ecmascript6.JSXHarmonyFileType
 import com.intellij.lang.javascript.JavascriptLanguage
-import com.intellij.lang.javascript.TypeScriptJSXFileType
-import com.intellij.lang.javascript.patterns.JSPatterns
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.xml.XmlAttributeValue
 import com.intellij.psi.xml.XmlTag
 
-internal class JsxTranslationExtractor: TranslationExtractor {
-    override fun canExtract(element: PsiElement): Boolean =
-        listOf(JSXHarmonyFileType.INSTANCE, TypeScriptJSXFileType.INSTANCE).any { it == element.containingFile.fileType } &&
-            PsiTreeUtil.getParentOfType(element, XmlTag::class.java)?.let {
-                !PsiTreeUtil.findChildOfType(it, XmlTag::class.java).toBoolean()
-            } ?: false
+internal class JsxTranslationExtractor : TranslationExtractor {
+    override fun canExtract(element: PsiElement): Boolean {
+        val fileType = element.containingFile.fileType
+        val fileName = element.containingFile.name
+        val isJsxFile = fileType.name.contains("JSX", ignoreCase = true) || fileName.endsWith(
+            ".jsx",
+            ignoreCase = true
+        ) || fileName.endsWith(".tsx", ignoreCase = true)
+
+        return isJsxFile && PsiTreeUtil.getParentOfType(element, XmlTag::class.java)?.let {
+            !PsiTreeUtil.findChildOfType(it, XmlTag::class.java).toBoolean()
+        } ?: false
+    }
 
     override fun isExtracted(element: PsiElement): Boolean =
-        element.isJs() && JSPatterns.jsArgument("t", 0).accepts(element.parent)
+        element.isJs() && com.intellij.lang.javascript.patterns.JSPatterns.jsArgument("t", 0).accepts(element.parent)
 
-    override fun text(element: PsiElement): String =
-        if (element.parent is XmlAttributeValue) element.text
-        else PsiTreeUtil.getParentOfType(element, XmlTag::class.java)!!
-            .value
-            .textElements
-            .map {it.text}
-            .joinToString(" ")
+    override fun text(element: PsiElement): String = if (element.parent is XmlAttributeValue) element.text
+    else PsiTreeUtil.getParentOfType(element, XmlTag::class.java)!!.value.textElements.map { it.text }.joinToString(" ")
 
     override fun textRange(element: PsiElement): TextRange =
         if (element.parent is XmlAttributeValue) element.parent.textRange
-        else PsiTreeUtil.getParentOfType(element, XmlTag::class.java)!!
-            .value
-            .textElements
-            .let {
-                TextRange(
-                        it.first().textRange.startOffset,
-                        it.last().textRange.endOffset
-                )
-            }
+        else PsiTreeUtil.getParentOfType(element, XmlTag::class.java)!!.value.textElements.let {
+            TextRange(
+                it.first().textRange.startOffset, it.last().textRange.endOffset
+            )
+        }
 
     override fun template(element: PsiElement): (argument: String) -> String = {
         "{i18n.t($it)}"
     }
+
     private fun PsiElement.isJs(): Boolean = this.language == JavascriptLanguage.INSTANCE
 }
